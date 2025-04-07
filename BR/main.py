b25b050ed3a865e7db16c3d65a9a279087ca1563
Semenov Arseniy <semarsdmi@gmail.com>
2025-04-07 15:06:14 +0700

    добавил контроль угла поворота лица

--- BR/main.py
+++ BR/main.py
@@ -1,8 +1,17 @@
 import cv2
 import threading
 import matplotlib as plt
+from collections import deque
+from angle_fun import check_position
 from face_processor import FaceProcessor
 from signal_processing import SignalProcessor
+
+N = 5
+# Буферы для сглаживания ключевых точек
+landmark_buffers = [deque(maxlen=N) for _ in range(6)]
+yaw_buffer = deque(maxlen=N)
+pitch_buffer = deque(maxlen=N)
+roll_buffer = deque(maxlen=N)
 
 def main():
     face_processor = FaceProcessor()
@@ -13,6 +22,12 @@ def main():
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret: break
+            
+            if check_position(frame, landmark_buffers, yaw_buffer, pitch_buffer, roll_buffer):
+                cv2.putText(frame, "Correct!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            else:
+                cv2.putText(frame, "Change possition!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                continue
             processed_frame, rois = face_processor.process_frame(frame)
             if rois:
                 filtered_signal = signal_processor.process(frame, rois)
