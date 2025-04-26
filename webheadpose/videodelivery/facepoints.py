113e0523e2e9ea95033ef4f93614bc69eb00bc75
lerusiksimpopulik <144207170+lerusiksimpopulik@users.noreply.github.com>
2025-04-27 03:00:41 +0700

    add web-app with livestream and arina's and arseniy's work

---
+++ webheadpose/videodelivery/facepoints.py
@@ -0,0 +1,84 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=True)
+BREATHING_LANDMARKS = [
+    1,   # Кончик носа
+    175, # Подбородок
+    8,   # Середина между глазами
+    48, 278,
+    115, 220, 45,
+    344, 440, 275,
+    236, 456, # Переносица
+    209, 429,
+]
+CHEEK_RIGHT = [34, 58, 64, 47]
+CHEEK_LEFT = [264, 288, 294, 277]
+BETWEEN_EYEBROWS = [107, 55, 285, 336]
+NOSE_BRIDGE = [114, 115, 278, 277]
+FACE_TRACKING = [1, 199, 33, 263, 61, 291]
+
+
+def extract_all_landmarks(frame):
+    results = face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    landmarks = []
+    h, w, _ = frame.shape
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for landmark in face_landmarks.landmark:
+                x, y = landmark.x * w, landmark.y * h
+                landmarks.append((x, y))
+    return landmarks
+
+def get_breathing_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx, landmark in enumerate(all_landmarks):
+        if (idx in BREATHING_LANDMARKS) and (landmark not in showing_landmarks):
+            showing_landmarks.append(landmark)
+    return showing_landmarks
+
+def get_nose_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx, landmark in enumerate(all_landmarks):
+        if (idx in NOSE_BRIDGE) and (landmark not in showing_landmarks):
+            showing_landmarks.append(landmark)
+    return showing_landmarks
+
+def get_eyebrows_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx, landmark in enumerate(all_landmarks):
+        if (idx in BETWEEN_EYEBROWS) and (landmark not in showing_landmarks):
+            showing_landmarks.append(landmark)
+    return showing_landmarks
+
+def get_cheeckL_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx, landmark in enumerate(all_landmarks):
+        if (idx in CHEEK_LEFT) and (landmark not in showing_landmarks):
+            showing_landmarks.append(landmark)
+    return showing_landmarks
+
+def get_cheeckR_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx, landmark in enumerate(all_landmarks):
+        if (idx in CHEEK_RIGHT) and (landmark not in showing_landmarks):
+            showing_landmarks.append(landmark)
+    return showing_landmarks
+
+def get_face_tracking_landmarks(all_landmarks):
+    showing_landmarks = []
+    for idx in FACE_TRACKING:
+        showing_landmarks.append(all_landmarks[idx])
+    return showing_landmarks
+
+#Если надо достать точки на той части лица, которых нет в списке поддерживаемых
+def get_custom_landmarks(landmarks, *selected_indices_arrays):
+    showing_landmarks = []
+    for idx, landmark in enumerate(landmarks):
+        for selected_indices in selected_indices_arrays:
+            if (idx in selected_indices) and (landmark not in showing_landmarks):
+                showing_landmarks.append(landmark)
+    return showing_landmarks
---
+++ webheadpose/videodelivery/views.py
@@ -0,0 +1,128 @@
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+from collections import deque
+
+import cv2
+import numpy as np
+
+from . import facepoints
+
+
+# Класс обработки кадров(код арины)
+class FrameProcessor:
+    def __init__(self):
+        self.N = 5
+        self.landmark_buffers = [deque(maxlen=self.N) for _ in range(6)]
+        self.yaw_buffer = deque(maxlen=self.N)
+        self.pitch_buffer = deque(maxlen=self.N)
+        self.roll_buffer = deque(maxlen=self.N)
+
+        # === Параметры камеры ===
+        self.model_points = np.array([
+            (0.0, 0.0, 0.0),  # 1. Кончик носа
+            (0.0, -330.0, -65.0),  # 2. Подбородок
+            (-225.0, 170.0, -135.0),  # 3. Левый глаз (внешний угол)
+            (225.0, 170.0, -135.0),  # 4. Правый глаз (внешний угол)
+            (-150.0, -150.0, -125.0),  # 5. Левый угол рта
+            (150.0, -150.0, -125.0)  # 6. Правый угол рта
+        ], dtype=np.float64)
+
+        focal_length = 1.10851252e+03
+        center = (640, 360)
+        self.camera_matrix = np.array([
+            [focal_length, 0, center[0]],
+            [0, focal_length, center[1]],
+            [0, 0, 1]
+        ], dtype=np.float64)
+
+    def process(self, frame):
+        raw_points = facepoints.extract_all_landmarks(frame)
+        my_points = []
+        if raw_points:
+            my_points = facepoints.get_face_tracking_landmarks(raw_points)
+
+        if len(my_points) != 6:
+            return frame  # Нет всех точек — просто вернём кадр как есть
+
+        for j, point in enumerate(my_points):
+            self.landmark_buffers[j].append(point)
+
+        if not all(len(buf) > 0 for buf in self.landmark_buffers):
+            return frame  # Буферы ещё не накопились
+
+        image_points = np.array([
+            np.mean(self.landmark_buffers[j], axis=0) for j in range(6)
+        ], dtype=np.float64)
+
+        # Решаем PnP
+        _, rotation_vector, _ = cv2.solvePnP(
+            self.model_points, image_points, self.camera_matrix, None, flags=cv2.SOLVEPNP_SQPNP)
+
+        rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
+        angles, _, _, _, _, _ = cv2.RQDecomp3x3(rotation_matrix)
+        yaw, pitch, roll = angles[1], angles[0], angles[2]
+
+        if pitch > 90:
+            pitch = 180 - pitch
+        elif pitch < -90:
+            pitch = -(180 + pitch)
+
+        self.yaw_buffer.append(yaw)
+        self.pitch_buffer.append(pitch)
+        self.roll_buffer.append(roll)
+
+        smooth_yaw = np.median(self.yaw_buffer)
+        smooth_pitch = np.median(self.pitch_buffer)
+        smooth_roll = np.median(self.roll_buffer)
+
+        # Вывод текста на кадр
+        cv2.putText(frame, f"Yaw: {smooth_yaw:.2f}", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        cv2.putText(frame, f"Pitch: {smooth_pitch:.2f}", (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        cv2.putText(frame, f"Roll: {smooth_roll:.2f}", (30, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        if (abs(smooth_yaw) <= 10 and smooth_yaw <= 3 and abs(smooth_pitch) <= 7 and abs(smooth_roll) <= 10):
+            cv2.putText(frame, "Correct!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, "Change position!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+        return frame
+
+
+# Генератор кадров
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError('Не удалось открыть видеопоток')
+
+    processor = FrameProcessor()
+
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if not success or frame is None:
+            continue
+
+        frame = processor.process(frame)
+
+        if frame is None:
+            continue
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            continue
+
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+
+# Страница index.html
+def index(request):
+    return render(request, 'index.html')
+
+# Поток видео
+def video_feed(request):
+    return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
