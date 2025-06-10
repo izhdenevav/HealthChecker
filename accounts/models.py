281e7c1f14bc029d4c518971ab91d55fe63da054
Valeria Chernodolya <chernodolja@gmail.com>
2025-06-10 13:54:31 +0700

    Update models.py
    add comments

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Модель для хранения измерений пульса и дыхания пользователя
 class HealthMeasurement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Связь с пользователем
     date = models.DateTimeField(verbose_name="Дата и время измерения")
     hr = models.PositiveSmallIntegerField(verbose_name="Пульс (уд/мин)")
     br = models.PositiveSmallIntegerField(verbose_name="Дыхание (дых/мин)")
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['-date'] # Последние измерения первыми
         verbose_name = 'Измерение'
         verbose_name_plural = 'Измерения'
 
