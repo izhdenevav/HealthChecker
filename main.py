1f65bda38e2220dc05c989084d944cc342cf973d
izhdenevav <vizdeneva@gmail.com>
2025-06-10 12:47:18 +0700

    add comments to decompose_module.py

--- main.py
+++ main.py
@@ -8,18 +8,8 @@ from scipy.signal import find_peaks
 from data_preprocessing import apply_color_space_conversion, apply_time_domain_normalization, add_white_noise
 from decompose_module import DCTiDCT
 from srrn import SRRN
-# from angle_fun import check_position
-
-# N = 5
-# # Буферы для сглаживания ключевых точек
-# landmark_buffers = [deque(maxlen=N) for _ in range(6)]
-# yaw_buffer = deque(maxlen=N)
-# pitch_buffer = deque(maxlen=N)
-# roll_buffer = deque(maxlen=N)
 
 def main():
-    # face_processor = FaceProcessor()
-    # signal_processor = SignalProcessor(warmup_frames=45)
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     if not cap.isOpened():
         print("Error: Could not open camera")
@@ -45,21 +35,6 @@ def main():
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             
-            # if check_position(frame, landmark_buffers, yaw_buffer, pitch_buffer, roll_buffer):
-            #     cv2.putText(frame, "Correct!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-            # else:
-            #     cv2.putText(frame, "Change possition!", (30, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-            #     continue
-            
-            # processed_frame, rois = face_processor.process_frame(frame)
-            # if rois:
-                # filtered_signal = signal_processor.process(frame, rois)
-            # status_color = (0, 255, 0) if signal_processor.is_ready else (0, 0, 255)
-            # cv2.imshow("Face ROI (Final)", processed_frame)
-            # cv2.putText(processed_frame, 
-                       # f"Status: {'ANALYZING' if signal_processor.is_ready else 'WARMUP'}",
-                       # (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, status_color, 2)
-
             if frame_count <= number_of_frames_per_calculation:
                 csc = apply_color_space_conversion(frame)
                 spatial_temporal_map.append(csc)
